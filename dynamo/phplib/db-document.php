fc94540b372c714a9340d820b7141fefb1271efe
Michael Sweet <msweet@apple.com>
2014-11-06 18:03:27 -0500

    Update DB uploads.

--- dynamo/phplib/db-document.php
+++ dynamo/phplib/db-document.php
@@ -398,19 +398,19 @@ class document
     if (array_key_exists("workgroup_id", $_POST))
       $this->workgroup_id = (int)$_POST["workgroup_id"];
 
-    if (array_key_exists("editable_file", $_FILES))
+    if (array_key_exists("editable_file", $_FILES) && $_FILES["editable_file"]["tmp_name"])
       if ($url = $this->upload($_FILES["editable_file"], $error))
         $this->editable_url = $url;
       else
         return (FALSE);
 
-    if (array_key_exists("redline_file", $_FILES))
+    if (array_key_exists("redline_file", $_FILES) && $_FILES["redline_file"]["tmp_name"])
       if ($url = $this->upload($_FILES["redline_file"], $error))
         $this->redline_url = $url;
       else
         return (FALSE);
 
-    if (array_key_exists("clean_file", $_FILES))
+    if (array_key_exists("clean_file", $_FILES) && $_FILES["clean_file"]["tmp_name"])
       if ($url = $this->upload($_FILES["clean_file"], $error))
         $this->clean_url = $url;
       else
