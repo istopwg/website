7480ffb435e91e593b7421e5ca7a2303542c8353
Michael Sweet <msweet@msweet-imac.local>
2015-08-12 14:35:20 -0400

    Update BOFs, IPP pages, wrapper script to support images.

--- dynamo/wrap.php
+++ dynamo/wrap.php
@@ -13,6 +13,14 @@
 
 include_once "phplib/site.php";
 
+if (strpos($PATH_INFO, "..") !== FALSE || !file_exists($PATH_TRANSLATED))
+{
+  header("Status: 404");
+  print("<h1>File Not Found</h1>\n");
+  print("<p>The file \"" . htmlspecialchars($PATH_INFO) . "\" was not found.</p>\n");
+  exit();
+}
+
 // Get the contents and metadata from the base HTML file...
 $title    = htmlspecialchars(basename($PATH_INFO, ".html"), ENT_QUOTES);
 $content  = "";
@@ -29,8 +37,35 @@ else if ($PATH_INFO == "${SITE_DOCROOT}standards.html")
   include_once "standards.php";
   exit(0);
 }
-else if (file_exists($PATH_TRANSLATED))
+else
 {
+  // Tag image files appropriately...
+  if (preg_match("/\\.gif\$/", $PATH_TRANSLATED))
+  {
+    header("Content-Type: image/gif");
+    readfile($PATH_TRANSLATED);
+    exit(0);
+  }
+  else if (preg_match("/\\.jpg\$/", $PATH_TRANSLATED))
+  {
+    header("Content-Type: image/jpeg");
+    readfile($PATH_TRANSLATED);
+    exit(0);
+  }
+  else if (preg_match("/\\.pdf\$/", $PATH_TRANSLATED))
+  {
+    header("Content-Type: application/pdf");
+    readfile($PATH_TRANSLATED);
+    exit(0);
+  }
+  else if (preg_match("/\\.png\$/", $PATH_TRANSLATED))
+  {
+    header("Content-Type: image/png");
+    readfile($PATH_TRANSLATED);
+    exit(0);
+  }
+
+  // Otherwise assume HTML...
   $contents = file_get_contents($PATH_TRANSLATED);
 
   if (($start = stripos($contents, "<style type=\"text/css\">")) !== FALSE)
@@ -67,17 +102,11 @@ else if (file_exists($PATH_TRANSLATED))
     $end = stripos($contents, "</body>", $start);
     $contents = substr($contents, $start + 6, $end - $start - 6);
   }
-}
-else
-{
-  // File does not exist, show a standard error page.
-  $title    = "Not Found";
-  $contents = "The file you requested cannot be found.";
-}
 
-// Wrap the contents of the HTML file with the standard header/footer for the site.
-site_header($title, $subtitle);
-print("$contents\n");
-site_footer();
+  // Wrap the contents of the HTML file with the standard header/footer for the site.
+  site_header($title, $subtitle);
+  print("$contents\n");
+  site_footer();
+}
 
 ?>
