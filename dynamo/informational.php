a09dc770b47ec7fd64a8697a18217e578b31512f
Michael R Sweet <michael.r.sweet@gmail.com>
2023-01-14 11:52:24 -0500

    Add informational document page.

---
+++ dynamo/informational.php
@@ -0,0 +1,22 @@
+<?php
+//
+// PWG home page.
+//
+
+include_once "phplib/site.php";
+include_once "phplib/db-document.php";
+
+site_header("Informational Documents", "", TRUE);
+
+$matches = document_search("", "number", 5199, DOCUMENT_STATUS_INFORMATIONAL);
+
+foreach ($matches as $id)
+{
+  $doc = new document($id);
+
+  $doc->view("", 2, FALSE);
+}
+
+site_footer();
+
+?>
